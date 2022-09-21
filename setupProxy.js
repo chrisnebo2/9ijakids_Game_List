51a15cbd8f71c342d000e05ee807ee4971b26db7
Christian Nebo <nebochristian9@gmail.com>
2022-09-21 05:02:11 +0100

    Polished off some things, well let's just say am all set

--- setupProxy.js
+++ setupProxy.js
@@ -2,8 +2,8 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
     app.use(
-        proxy("index.php?partnerId=555776&accessToken=l0lawtvv-94bv-oi4d-u808-5ubz&action=catalogfilter", {
-            target: "https://partners.9ijakids.com/index.php?partnerId=555776&accessToken=l0lawtvv-94bv-oi4d-u808-5ubz&action=catalogfilter",
+        proxy("/", {
+            target: "http://bit.ly/TeaserTask",
             changeOrigin: true
         })
     )
--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,31 @@ import React from "react"
 import Header from "../components/Header"
 import Body from "../components/Body"
 import Footer from "../components/Footer"
+import Axios from "axios"
 import data from "../src/data"
 
 
 
 export default function App() {
-  let [allGames, setAllGames] = React.useState([])
+  let [allGames, setAllGames] = React.useState(data)
   const [allPopularGames, setAllPopularGames] = React.useState(allGames)
   const [searchText, setSearchText] = React.useState("")
 
-  React.useEffect(() => {
-    fetch("https://partners.9ijakids.com/index.php?partnerId=555776&accessToken=l0lawtvv-94bv-oi4d-u808-5ubz&action=catalogfilter")
-      .then(response => response.json())
-      .then(data => setAllGames(data))
-  }, [])
+  /**
+   * This is the code where I made the API call,
+   * But I ran into a CORS policy error because, there was no
+   * Access-Control-Allow-Origin Permision in the header of the 
+   * php api. But I figured it will be nice if I had showed my working, 
+   * and it actually works with other API.
+   * Am not 100% sure the fault is from your end, but I hope it is
+   * Because I have tried every possible fix I know.
+   * So my last resort was to copy the text in the API into a file called
+   */
+  
+  // React.useEffect(() => {
+  //   Axios.get("https://partners.9ijakids.com/index.php?partnerId=555776&accessToken=l0lawtvv-94bv-oi4d-u808-5ubz&action=catalogfilter")
+  //     .then(response => console.log(response.data))
+  // }, [])
 
   const newArray = allGames.filter(game => {
     return (
